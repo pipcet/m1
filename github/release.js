3e8712bd8bb0f3dddaa95e39b39bba85d993c4f1
Pip Cet <pipcet@gmail.com>
2021-05-03 08:11:17 +0000

    oops, fix dummy release

--- github/release.js
+++ github/release.js
@@ -4,9 +4,6 @@ let last_release_date = process.argv[3];
 //last_release_date = last_release_date.substr(1, this_release_date.length - 2)
 let child_process = require("child_process");
 let log = "";
-let last_hash = child_process.execSync(`git rev-parse ${last_release_date}`).toString();
-while (last_hash.length && last_hash[last_hash.length-1] === "\n")
-    last_hash = last_hash.substr(0, last_hash.length-1);
 let json = {};
 json.tag_name = this_release_date;
 json.name = `${this_release_date} (automatic release)`;
