2080cf8f50108fdf0955b9f895d6b6dc2755bcff
Pip Cet <pipcet@gmail.com>
2021-05-14 12:19:18 +0000

    macos-code/lsh.c: timestamp shared page

--- macos-code/lsh.c
+++ macos-code/lsh.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 int main(int argc, char **argv)
 {
@@ -11,11 +12,16 @@ int main(int argc, char **argv)
   strcpy(page + 1024, argv[1]);
   page[0] = 0x5a6b448a98b350b6;
   page[3] = 0;
-  while (page[4])
+  while (page[4]) {
     page[1]++;
+    page[5] = time(NULL);
+  }
   page[4] = 'C';
-  while (page[4])
+  while (page[4]) {
     page[1]++;
+    page[5] = time(NULL);
+  }
   write(1, page + 1024, page[3]);
+  page[0] = 0;
   exit(0);
 }
