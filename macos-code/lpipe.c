1c492b5dc4fa23d3f58e7181964e6afcb758efa6
Pip Cet <pipcet@gmail.com>
2021-05-24 18:12:28 +0000

    macos-code/lpipe.c: add generous synchronization

--- macos-code/lpipe.c
+++ macos-code/lpipe.c
@@ -14,15 +14,27 @@ int main(int argc, char **argv)
   strcpy(page + 1024, argv[1]);
   page[0] = 0x5a6b448a98b350b6;
   page[3] = 0;
+    asm volatile("isb");
+    asm volatile("dmb sy");
+    asm volatile("dsb sy");
+    asm volatile("isb");
   while (page[4]) {
     page[1]++;
     page[5] = time(NULL);
   }
   page[4] = '|';
+    asm volatile("isb");
+    asm volatile("dmb sy");
+    asm volatile("dsb sy");
+    asm volatile("isb");
   while (page[4]) {
     page[1]++;
     page[5] = time(NULL);
   }
+    asm volatile("isb");
+    asm volatile("dmb sy");
+    asm volatile("dsb sy");
+    asm volatile("isb");
   while (true) {
     while (page[4]) {
       page[1]++;
@@ -32,10 +44,8 @@ int main(int argc, char **argv)
     FD_ZERO(&readfds);
     FD_SET(0, &readfds);
     struct timeval timeout = { 0, };
-    printf("select\n");
     if (select(1, &readfds, NULL, NULL, &timeout)) {
       ssize_t ret = read(0, page + 1024, 8192);
-      printf("local -> remote %ld bytes\n", (long) ret);
       if (ret > 0) {
 	page[3] = ret;
 	page[4] = '>';
@@ -43,17 +53,27 @@ int main(int argc, char **argv)
 	  page[1]++;
 	  page[5] = time(NULL);
 	}
+    asm volatile("isb");
+    asm volatile("dmb sy");
+    asm volatile("dsb sy");
+    asm volatile("isb");
 	continue;
       }
     }
-    printf("remote -> local\n");
     page[3] = 0;
+    asm volatile("isb");
+    asm volatile("dmb sy");
+    asm volatile("dsb sy");
+    asm volatile("isb");
     page[4] = '<';
     while (page[4]) {
       page[1]++;
       page[5] = time(NULL);
     }
-    printf("remote -> local read %ld bytes\n", page[3]);
+    asm volatile("isb");
+    asm volatile("dmb sy");
+    asm volatile("dsb sy");
+    asm volatile("isb");
     if (page[3])
       write(1, page + 1024, page[3]);
   }
